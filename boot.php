6110badb9e89e64ed8ce3ba6c3aadd3b072904d2
eaCe <webmaster@slaps.de>
2023-12-10 08:52:54 +0100

    fix: prevent warning

--- boot.php
+++ boot.php
@@ -1,14 +1,15 @@
 <?php
 
 $addon = rex_addon::get('module_styles');
-$styles = explode("\r\n", $addon->getConfig('module_stylesheet'));
 
 if (rex::isBackend() && rex::getUser() && rex_url::currentBackendPage() === 'index.php?page=content/edit' && $addon->getConfig('module_stylesheet')) {
+    $styles = explode("\r\n", $addon->getConfig('module_stylesheet'));
     rex_view::setJsProperty('moduleStylesheet', $styles);
     rex_view::addJsFile($addon->getAssetsUrl('module-styles.js'));
 }
 
 if (rex::isBackend() && rex::getUser() && rex_url::currentBackendPage() === 'index.php?page=module_styles/settings' && $addon->getConfig('module_stylesheet')) {
+    $styles = explode("\r\n", $addon->getConfig('module_stylesheet'));
     rex_view::setJsProperty('moduleStylesheet', $styles);
     rex_view::addJsFile($addon->getAssetsUrl('module-styles-settings.js'));
 }
